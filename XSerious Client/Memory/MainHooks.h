ad7d5e0526ec20b759aab7ef0d451c8f1f5a906c
yeemi routh <shade4keal@gmail.com>
2022-02-21 12:07:18 +1100

    forgot to remove that test func

--- XSerious Client/Memory/MainHooks.h
+++ XSerious Client/Memory/MainHooks.h
@@ -7,10 +7,6 @@ private:
 	typedef int(__cdecl* Sub_Print)(int, const char*);
 	Sub_Print RPrint = (Sub_Print)x(0x752820);
 
-	//int __fastcall sub_5EEA80(int a1, int a2, int a3, unsigned int a4, int a5)
-	typedef int(__cdecl* Sub_Print)(int a1, int a2, int a3, unsigned int a4, int a5);
-	Sub_Print Print = (Sub_Print)x(0x752820);
-
 public:
 	const void Print(const char* str) { RPrint(0, str); }
 	const void Alert(const char* str) { RPrint(1, str); }
